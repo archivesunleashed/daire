d6f00d49e4deb9ee63131140871d53601ea8a8c0
x65han <x65han@edu.uwaterloo.ca>
2020-01-30 20:09:35 -0500

    preliminary setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# AUT Demo
+
+
+### Scalability
+[Large dataset with hnswlib](https://github.com/nmslib/hnswlib/issues/81)
---
+++ server.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, jsonify, send_from_directory
+from backend.preprocess import preprocess
+app = Flask(__name__, static_folder='ui/build')
+
+# API
+@app.route('/gen', methods=["GET"])
+def generateImages():
+    return jsonify(isReady=True)
+
+
+# Serve React App
+@app.route('/', defaults={'path': 'index.html'})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+# Trigger Server
+if __name__ == '__main__':
+    # Pre-processing
+    preprocess()
+    # Development Mode
+    app.run(debug=True)
+    # Production Mode
+    # app.run(host='0.0.0.0', port=8080)
